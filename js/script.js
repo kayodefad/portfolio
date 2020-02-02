eddf9b554d885516ce1a1dc81311b6ea5c60525b
Oluwakayode <fadojuk@gmail.com>
2020-02-02 20:07:06 +0100

    Added smooth scrolling

--- js/script.js
+++ js/script.js
@@ -11,3 +11,19 @@ document.addEventListener('click', (e) => {
     menu.style.display = "none"
   }
 })
+
+// Smooth Scrolling
+$('a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault()
+
+    const hash = this.hash
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800
+    )
+  }
+})
