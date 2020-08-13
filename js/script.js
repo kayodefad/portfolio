b7befa108254ec85fe95ec0911d6a462950f5f98
Oluwakayode Fadoju <ofadoju@axamansard.com>
2020-08-13 22:15:36 +0100

    Change active menu on page scroll

--- js/script.js
+++ js/script.js
@@ -1,29 +1,57 @@
-let menu = document.querySelector('#navbar #menu-container')
+let menu = document.querySelector("#navbar #menu-container");
 
-let hamburger = document.querySelector('#hamburger-menu')
+let hamburger = document.querySelector("#hamburger-menu");
 
-hamburger.addEventListener('click', () => {
-  menu.style.display = 'block'
-})
+hamburger.addEventListener("click", () => {
+  menu.style.display = "block";
+});
 
-document.addEventListener('click', (e) => {
-  if(e.target == menu) {
-    menu.style.display = "none"
+document.addEventListener("click", e => {
+  if (e.target == menu) {
+    menu.style.display = "none";
   }
-})
+});
 
 // Smooth Scrolling
-$('a').on('click', function(event) {
-  if (this.hash !== '') {
-    event.preventDefault()
+$("a").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
 
-    const hash = this.hash
+    const hash = this.hash;
 
-    $('html, body').animate(
+    $("html, body").animate(
       {
-        scrollTop: $(hash).offset().top
+        scrollTop: $(hash).offset().top,
       },
-    800
-    )
+      800
+    );
   }
-})
+});
+
+// Change active menu item on scroll
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var topMenu = $(window).scrollTop();
+
+    const menuItems = ["about", "projects", "skills", "education", "contact"];
+
+    menuItems.forEach((item, i) => {
+      if (i == 0) {
+        if (topMenu >= $(`#${item}`).offset().top) {
+          // If you have scrolled past section one do this.
+          $(`.${item}-menu`).addClass("nav-active"); // Adds class of current-menu-item to the menu item with a class of menu-item-1
+        } else {
+          // If you have not scrolled section one do this.
+          $(`.${item}-menu`).removeClass("nav-active"); // Removes class of current-menu-item to the menu item with a class of menu-item-1
+        }
+      } else {
+        if (topMenu >= $(`#${menuItems[i - 1]}`).offset().top) {
+          $(`.${item}-menu`).addClass("nav-active");
+          $(`.${menuItems[i - 1]}-menu`).removeClass("nav-active");
+        } else {
+          $(`.${item}-menu`).removeClass("nav-active");
+        }
+      }
+    });
+  });
+});
